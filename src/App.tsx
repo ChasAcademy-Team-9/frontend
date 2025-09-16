100b48a87baed1f95d9d99097d61d1c95cea1969
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-09-16 11:02:03 +0200

    install react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
   return (
     <>
-      <h1 className='text-6xl'>hej</h1>
+      <h1 className='text-6xl'>
+        hej <FaBeer />
+      </h1>
     </>
   );
 }

aac9fd82ae3ee5ad8e4ef42fc8275276c6bb5f38
teo-02 <krickateodora@gmail.com>
2025-09-22 19:23:01 +0200

    Fixed small problems in index, app and css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import Position from "./routes/Position";
-import Scan from "./routes/Scan";
-import BottomNav from "./components/BottomNav";
-=======
-import { FaBeer } from 'react-icons/fa';
-import { PrimaryButton } from './components/PrimaryButton/PrimaryButton';
-import { SecondaryButton } from './components/SecondaryButton/SecondaryButton';
->>>>>>> development
+import { Outlet } from 'react-router-dom';
+import BottomNav from './components/BottomNav';
 
 function App() {
   return (
- <Router>
-      <div id="root">
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/position" element={<Position/>} />
-          <Route path="/scan" element={<Scan/>} />
-        </Routes>
-        <BottomNav />
-      </div>
-    </Router>
+    <div className="app">
+      <Outlet/>
+      <BottomNav />
+    </div>
   );
 }
+
 export default App;
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -12,14 +12,20 @@ import Home from './routes/Home.tsx';
 
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/sign-up', element: <SignUp /> },
-  { path: '/login', element: <Login /> },
-  { path: '*', element: <NotFound /> },
-  { path: '/scan', element: <Scan /> },
-  { path: '/position', element: <Position /> },
-  { path: '/home', element: <Home /> },
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: 'sign-up', element: <SignUp /> },
+      { path: 'login', element: <Login /> },
+      { path: 'scan', element: <Scan /> },
+      { path: 'position', element: <Position /> },
+      { path: 'home', element: <Home /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
 ]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

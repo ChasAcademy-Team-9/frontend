e1aa444ca8c3527e8b83fcf5aadd5cc24c6afc23
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-09-22 12:51:26 +0200

    Styling for header nav bar

--- src/components/HeaderNav/HeaderNav.tsx
+++ src/components/HeaderNav/HeaderNav.tsx
@@ -3,16 +3,16 @@ import { FaHome, FaMapMarkerAlt, FaQrcode } from 'react-icons/fa';
 
 export function HeaderNav() {
     return (
-        <nav className="flex gap-6">
-            <Link to="/" className="flex">
+        <nav className="flex gap-6 justify-center bg-secondary rounded p-1">
+            <Link to="/" className="flex gap-1 items-center bg-primary text-text-light py-1 px-3 rounded">
                 <FaHome />
                 Home
             </Link>
-            <Link to="/scan" className="flex">
+            <Link to="/scan" className="flex gap-1 items-center bg-primary text-text-light py-1 px-3 rounded">
                 <FaQrcode />
                 Scan
             </Link>
-            <Link to="/postion" className="flex">
+            <Link to="/postion" className="flex gap-1 items-center bg-primary text-text-light py-1 px-3 rounded">
                 <FaMapMarkerAlt />
                 Position
             </Link>

c14e9d351773d8f92af6bb8790f2bcef9f5c0fcc
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-09-22 14:03:51 +0200

    Add sign up page content.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,36 @@
+import Input from "../components/Input";
+import { PrimaryButton } from "../components/PrimaryButton/PrimaryButton";
+
 function SignUp() {
   return (
     <>
-      <h1>Regristera dig här</h1>
+      <button>⬅️</button> {/* TODO byt till backarrow-komponenten när den är klar */}
+
+      <h1>Skapa konto</h1>
+
+      <Input label="Email" name="Email" id="email" type="email" />
+
+      <Input label="Lösenord" name="password" id="password" type="password" />
+
+      <fieldset className="flex flex-col border-1 ">
+        <legend>
+          Kontotyp
+        </legend>
+        <label>
+          <input type="radio" name="role" />
+          Logistik
+        </label>
+        <label>
+          <input type="radio" name="role" />
+          Spårning
+        </label>
+        <label>
+          <input type="radio" name="role" />
+          Data
+        </label>
+      </fieldset>
+
+      <PrimaryButton text="Registrera" onClick={()=>null} />
     </>
   );
 }

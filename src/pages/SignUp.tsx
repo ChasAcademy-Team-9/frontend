ae7f38066001ac61f44d42f414cc39f2183b4074
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-09-22 16:56:35 +0200

    WIP

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,7 +4,7 @@ import { PrimaryButton } from "../components/PrimaryButton/PrimaryButton";
 function SignUp() {
   return (
     <>
-      <button>⬅️</button> {/* TODO byt till backarrow-komponenten när den är klar */}
+      <button className="text-6xl font-bold border rounded-full p-6">←</button> {/* TODO byt till backarrow-komponenten när den är klar */}
 
       <h1>Skapa konto</h1>
 
@@ -12,8 +12,20 @@ function SignUp() {
 
       <Input label="Lösenord" name="password" id="password" type="password" />
 
-      <fieldset className="flex flex-col border-1 ">
-        <legend>
+      <fieldset className="flex flex-col border-1 
+                         px-3 py-2 
+                         bg-surface   
+                         border border-border 
+                         rounded-2xl
+                         text-text-dark
+                         placeholder:text-able
+                         focus:outline-none 
+                         focus:border-focus 
+                         focus:ring-focus focus:ring-1
+                         cursor-text   
+                         disabled:cursor-not-allowed       
+               ">
+        <legend className="text-text-dark text-sm">
           Kontotyp
         </legend>
         <label>

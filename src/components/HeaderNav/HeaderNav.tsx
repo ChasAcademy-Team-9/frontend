4e5576cd39623bb72d785d0d83e99680363bc18b
Benjamin S <benjamin.stenlund@chasacademy.se>
2025-09-22 19:07:10 +0200

    Increase padding
    Co-authored-by: Jonte <180638563+jont-ctrl@users.noreply.github.com>

--- src/components/HeaderNav/HeaderNav.tsx
+++ src/components/HeaderNav/HeaderNav.tsx
@@ -3,7 +3,7 @@ import { FaHome, FaMapMarkerAlt, FaQrcode } from 'react-icons/fa';
 
 export function HeaderNav() {
     return (
-        <nav className="flex gap-6 justify-center bg-secondary rounded p-1">
+        <nav className="flex gap-6 justify-center bg-secondary rounded p-2">
             <NavItem to="/">
                 <FaHome />
                 Home
@@ -22,7 +22,7 @@ export function HeaderNav() {
 
 function NavItem({ to, children }: { to: string; children: React.ReactNode }){
     return (
-        <Link to={ to } className="flex gap-1 items-center bg-primary text-text-light py-1 px-3 rounded">
+        <Link to={ to } className="flex gap-1 items-center bg-primary text-text-light py-2 px-4 rounded">
             { children }
         </Link>
     )

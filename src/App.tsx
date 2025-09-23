b53dcb93e6cedf3499d262e8144b9f6855175440
atfedmis <federicamisirocchi@hotmail.com>
2025-09-23 09:37:24 +0200

    login screen just to test it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { FaBeer } from 'react-icons/fa';
 import { PrimaryButton } from './components/PrimaryButton/PrimaryButton';
 import { SecondaryButton } from './components/SecondaryButton/SecondaryButton';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <h1 className='text-6xl'>
-        hej <FaBeer />
-      </h1>
+      <Login />
     </>
   );
 }

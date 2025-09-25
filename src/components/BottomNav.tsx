c874d7b8e64711118d82b64fa2df8033ed20b40d
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-09-25 14:30:06 +0200

    add text primary color icons

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -4,13 +4,16 @@ import { FaHome, FaMapMarkerAlt, FaQrcode } from 'react-icons/fa';
 export default function BottomNav() {
   return (
     <nav className='fixed bottom-0 left-0 w-full bg-background border-t border-primary flex justify-around items-center text-center py-4'>
-      <Link to='/' className='text-2xl hover:text-primary'>
+      <Link to='/' className='text-2xl text-primary hover:text-text-dark'>
         <FaHome />
       </Link>
-      <Link to='/scan' className='text-2xl hover:text-primary'>
+      <Link to='/scan' className='text-2xl text-primary hover:text-text-dark'>
         <FaQrcode />
       </Link>
-      <Link to='/position' className='text-2xl hover:text-primary'>
+      <Link
+        to='/position'
+        className='text-2xl text-primary hover:text-text-dark'
+      >
         <FaMapMarkerAlt />
       </Link>
     </nav>

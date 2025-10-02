8d843c1f8f1b06c2285411017fa3f603d3f9cc8b
atfedmis <federicamisirocchi@hotmail.com>
2025-10-02 13:41:07 +0200

    working on infocard w styling and logic before api

--- src/routes/Driver.tsx
+++ src/routes/Driver.tsx
@@ -1,9 +1,19 @@
+import InfoCard from "../components/Driver/InfoCard"
 
 
 const Driver = () => {
   return (
-    <div>Driver</div>
-  )
-}
+    <div className="min-h-screen bg-background p-4">
+        <InfoCard
+          title="Information"
+          items={[
+            { label: 'Tid:', value: '14:30' },
+            { label: 'Distans:', value: '40 km' },
+            { label: 'Hastighet:', value: '60 km/h' }
+          ]}
+        />
+      </div>   
+        );
+};
 
-export default Driver
+        export default Driver

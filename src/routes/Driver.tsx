2138676012e70e44e61d5df9ad0c625ea93f174e
atfedmis <federicamisirocchi@hotmail.com>
2025-10-03 10:06:38 +0200

    add Leaflet and React-Leaflet for map integration; implement NavigationCard and MapComponent

--- src/routes/Driver.tsx
+++ src/routes/Driver.tsx
@@ -1,13 +1,16 @@
 import Dashboard from "../components/Driver/Dashboard";
-import InfoCard from "../components/Driver/InfoCard"
+import InfoCard from "../components/Driver/InfoCard";
+import NavigationCard from "../components/Driver/NavigationCard";
 import { PrimaryButton } from "../components/PrimaryButton";
-
 
 const Driver = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-text-dark mb-6 bg-secondary/80 p-5">Förar Dashboard</h1>
-      <div className="min-h-screen bg-background p-4">
+      <h1 className="text-3xl font-bold text-text-dark mb-6 bg-secondary/80 p-5">
+        Förar Dashboard
+      </h1>
+      
+      <div className="min-h-screen bg-background p-4 space-y-6">
         <InfoCard
           title="Information"
           items={[
@@ -16,7 +19,8 @@ const Driver = () => {
             { label: 'Hastighet:', value: '60 km/h' }
           ]}
         />
-        <div className="mt-8 flex justify-center">
+        
+        <div className="flex justify-center">
           <div className="scale-125">
             <PrimaryButton
               text="Skanna för att lägga till"
@@ -24,31 +28,16 @@ const Driver = () => {
             />
           </div>
         </div>
-        <div className="mt-8 grid grid-cols-1 gap-4">
+        
+        <div className="grid grid-cols-1 gap-4">
           <Dashboard label="Temperatur" value={22} unit="°C" trend="up" />
           <Dashboard label="Luftfuktighet" value={60} unit="%" trend="down" />
-          <Dashboard label="Batterinivå " value={65} unit="%" trend="warning" />
+          <Dashboard label="Batterinivå" value={65} unit="%" trend="warning" />
         </div>
 
-        <div className="mt-8 flex justify-center">
-          <div className="scale-125">
-            <PrimaryButton
-              text="Visa Rutt"
-              onClick={() => alert('Rutt!')}
-            />
-          </div>
-        </div>
-        <div className="mt-8 flex justify-center">
-          <div className="scale-125">
-            <PrimaryButton
-              text="Lista med packet"
-              onClick={() => alert('Listan!')}
-            />
-          </div>
-        </div>
+        <NavigationCard />
       </div>
     </div>
-
   );
 };
 

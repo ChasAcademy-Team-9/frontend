b4652da16a39fa735920b7d498e1cab99ce38231
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-10-07 16:07:37 +0200

    Improve sender page

--- src/pages/sender/Sender.tsx
+++ src/pages/sender/Sender.tsx
@@ -17,36 +17,62 @@ export function Sender() {
           <p>Hantera dina paket och sensorer.</p>
         </div>
       </header>
-      <section>
-        <div>
-          {" "}
-          <FaPiggyBank /> <div> 0</div> <div>Totalt paket</div>
+      <div className="flex flex-col gap-8 bg-white/75 p-4 m-[-1rem] rounded-b-3xl rounded-t-2xl mb-1">
+        <section className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div>
+            {" "}
+            <FaPiggyBank /> <span> 0</span> <span>Totalt paket</span>
+          </div>
+          <div>
+            {" "}
+            <FaPiggyBank /> <span> 0</span> <span>Totalt paket</span>
+          </div>
+          <div>
+            {" "}
+            <FaPiggyBank /> <span> 0</span> <span>Totalt paket</span>
+          </div>
+          <div>
+            {" "}
+            <FaPiggyBank /> <span> 0</span> <span>Totalt paket</span>
+          </div>
+        </section>
+        <section>
+          <FaWandSparkles /> Du har 1 aktiv varning som behöver hanteras.
+        </section>
+        <PrimaryButton
+          text="+ Skapa nytt paket"
+          onClick={() => navigate("/sender/new")}
+          // onClick={() => navigate("new")}
+        />
+      </div>
+      <div className="flex flex-col gap-8 bg-white/75 p-4 m-[-1rem] rounded-b-3xl rounded-t-2xl">
+        <div className="flex justify-left items-baseline gap-4">
+          <h2 className="text-2xl">Mina paket</h2>
+          <span>3 paket</span>
         </div>
-        <div>
-          {" "}
-          <FaPiggyBank /> <div> 0</div> <div>Totalt paket</div>
-        </div>
-        <div>
-          {" "}
-          <FaPiggyBank /> <div> 0</div> <div>Totalt paket</div>
-        </div>
-        <div>
-          {" "}
-          <FaPiggyBank /> <div> 0</div> <div>Totalt paket</div>
-        </div>
-      </section>
-      <section>
-        <FaWandSparkles /> Du har 1 aktiv varning som behöver hanteras.
-      </section>
-      <PrimaryButton
-        text="+ Skapa nytt paket"
-        onClick={() => navigate("/sender/new")}
-        // onClick={() => navigate("new")}
-      />
-      <h2>Mina paket</h2>
-      <span>3 paket</span>
-      <Card variant="package" destination="Svenska Livs AB" paketId="PKG-123" />
-      <Card variant="package" />
+        <Card
+          variant="package"
+          destination="Svenska Livs AB"
+          paketId="PKG-123"
+          status="OK"
+        />
+        <Card
+          variant="package"
+          destination="Svenska Livs AB"
+          paketId="PKG-123"
+        />
+        <Card
+          variant="package"
+          destination="Svenska Livs AB"
+          paketId="PKG-123"
+          status="Kritisk"
+        />
+        <Card
+          variant="package"
+          destination="Svenska Livs AB"
+          paketId="PKG-123"
+        />
+      </div>
     </main>
   );
 }

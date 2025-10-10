f054f8f7792f7a7b0b2a4ac1f81578e5814e654e
atfedmis <federicamisirocchi@hotmail.com>
2025-10-10 12:34:46 +0200

    - add icon props and flex style to PrimaryButton component to accept and render an icon - update Scanning page to utilize new functionality

--- src/pages/Scanning.tsx
+++ src/pages/Scanning.tsx
@@ -1,8 +1,25 @@
+import { PrimaryButton } from "../components/PrimaryButton"
+import { IoMdQrScanner } from "react-icons/io"
+import { FaPlus } from "react-icons/fa";
+
 
 const Scanning = () => {
-  return (
-    <div>Scanning</div>
-  )
+     return (
+          <div>
+               <PrimaryButton
+                    text="Starta Skanning"
+                    icon={<IoMdQrScanner style={{ marginRight: '4px' }} />}
+                    onClick={() => console.log("Starta Skanning")}
+               >
+               </PrimaryButton>
+               <PrimaryButton
+                    text="Ange Manuellt"
+                    icon={<FaPlus style={{ marginRight: '4px' }} />}
+                    onClick={() => console.log("Ange Manuellt")}
+               >
+               </PrimaryButton>
+          </div>
+     )
 }
 
 export default Scanning

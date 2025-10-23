3c158b41a4d2e29d4b585e2bc95e59004dcaf3fd
atfedmis <federicamisirocchi@hotmail.com>
2025-10-23 13:30:37 +0200

    implement package service with methods to fetch all packages and package by ID

--- src/api/packageService.ts
+++ src/api/packageService.ts
@@ -1,1 +1,18 @@
- 
+import { apiClient } from "./apiClient";
+import type { PackageListResponse, PackageResponse } from "../types/package";
+
+export const packageService = {
+     // GET alla packet
+     getAllPackages: async (): Promise<PackageListResponse> => {
+          const response = await apiClient.get<PackageListResponse>
+               (`/api/packages`);
+          return response.data;
+     },
+
+     // GET ett packet via ID
+     getPackageID: async (packageID: number): Promise<PackageResponse> => {
+          const response = await apiClient.get<PackageResponse>(`/api/packages/${packageID}`);
+          return response.data;
+     }
+};
+

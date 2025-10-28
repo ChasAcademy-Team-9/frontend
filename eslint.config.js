f3c93fc3ed7ac5ce664d127c5574318e5f244402
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-10-28 14:33:12 +0100

    Add eslint-config-prettier
    Install and configure eslint and prettier to work together.

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,41 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from "vite";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
+export default defineConfig([
+  {
+    ignores: ["dist"], // properly ignore the dist folder
+  },
+  js.configs.recommended, // base JS rules
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
       globals: globals.browser,
     },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
   },
+  eslintConfigPrettier, // should be last
 ]);

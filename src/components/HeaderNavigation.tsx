713e4d4135c6a1e0b3c1d798dad24a372680b09d
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-10-31 09:31:51 +0100

    save id localstorage

--- src/components/HeaderNavigation.tsx
+++ src/components/HeaderNavigation.tsx
@@ -1,72 +1,73 @@
-import { useEffect, useState } from 'react';
-import { PrimaryButton } from './PrimaryButton';
-import { SecondaryButton } from './SecondaryButton';
-import { Link } from 'react-router-dom';
-import { LuCircleUserRound } from 'react-icons/lu';
+import { useEffect, useState } from "react";
+import { PrimaryButton } from "./PrimaryButton";
+import { SecondaryButton } from "./SecondaryButton";
+import { Link } from "react-router-dom";
+import { LuCircleUserRound } from "react-icons/lu";
 
 function HeaderNavigation() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
-  const [linkUrlProfile, setLinkUrlProfile] = useState('');
+  const [username, setUsername] = useState("");
+  const [linkUrlProfile, setLinkUrlProfile] = useState("");
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    const localUserName = localStorage.getItem('username');
-    const localRole = localStorage.getItem('role');
+    const token = localStorage.getItem("token");
+    const localUserName = localStorage.getItem("username");
+    const localRole = localStorage.getItem("role");
 
     if (token) {
       setLoggedIn(true);
-      setUsername(localUserName || '');
+      setUsername(localUserName || "");
 
-      if (localRole === 'driver') {
-        setLinkUrlProfile('/driver');
-      } else if (localRole === 'sender') {
-        setLinkUrlProfile('/sender');
-      } else if (localRole === 'receiver') {
-        setLinkUrlProfile('/recipient');
+      if (localRole === "driver") {
+        setLinkUrlProfile("/driver");
+      } else if (localRole === "sender") {
+        setLinkUrlProfile("/sender");
+      } else if (localRole === "receiver") {
+        setLinkUrlProfile("/recipient");
       }
     } else {
       setLoggedIn(false);
-      setUsername('');
-      setLinkUrlProfile('');
+      setUsername("");
+      setLinkUrlProfile("");
     }
   }, []);
 
   function handleLogOut() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('role');
-    localStorage.removeItem('username');
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    localStorage.removeItem("username");
+    localStorage.removeItem("id");
 
     setLoggedIn(false);
-    setUsername('');
-    setLinkUrlProfile('');
+    setUsername("");
+    setLinkUrlProfile("");
 
     window.location.reload();
   }
 
   return (
-    <nav className='flex justify-between items-center sm:gap-4 p-4 '>
-      <Link to='/'>
-        <h2 className='sm:text-2xl text-xl  font-bold text-primary'>Chas 9</h2>
+    <nav className="flex justify-between items-center sm:gap-4 p-4 ">
+      <Link to="/">
+        <h2 className="sm:text-2xl text-xl  font-bold text-primary">Chas 9</h2>
       </Link>
       {loggedIn ? (
-        <div className='flex justify-center items-center sm:gap-4'>
+        <div className="flex justify-center items-center sm:gap-4">
           <Link to={linkUrlProfile}>
-            <button className='text-xl flex justify-center items-center gap-1 hover:bg-secondary brightness-150 hover:underline px-6 py-2 rounded-2xl'>
+            <button className="text-xl flex justify-center items-center gap-1 hover:bg-secondary brightness-150 hover:underline px-6 py-2 rounded-2xl">
               <LuCircleUserRound />
               {username}
             </button>
           </Link>
 
-          <SecondaryButton text='Logga ut' onClick={handleLogOut} />
+          <SecondaryButton text="Logga ut" onClick={handleLogOut} />
         </div>
       ) : (
-        <div className='flex justify-center items-center gap-2'>
-          <Link to='/sign-up'>
-            <PrimaryButton text='Registrera' />
+        <div className="flex justify-center items-center gap-2">
+          <Link to="/sign-up">
+            <PrimaryButton text="Registrera" />
           </Link>
-          <Link to='/login'>
-            <SecondaryButton text='Logga in' />
+          <Link to="/login">
+            <SecondaryButton text="Logga in" />
           </Link>
         </div>
       )}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,18 @@
-import Input from '../components/Input';
-import { PrimaryButton } from '../components/PrimaryButton';
-import BackArrow from '../components/BackArrow';
-import { Link } from 'react-router-dom';
-import { Dropdown } from '../components/Dropdown';
-import { useState } from 'react';
-import HeaderNavigation from '../components/HeaderNavigation';
-import LoadingAnimation from '../components/LoadingAnimation';
-import { useNavigate } from 'react-router';
+import Input from "../components/Input";
+import { PrimaryButton } from "../components/PrimaryButton";
+import BackArrow from "../components/BackArrow";
+import { Link } from "react-router-dom";
+import { Dropdown } from "../components/Dropdown";
+import { useState } from "react";
+import HeaderNavigation from "../components/HeaderNavigation";
+import LoadingAnimation from "../components/LoadingAnimation";
+import { useNavigate } from "react-router";
 
 function Login() {
-  const [statusMsg, setStatusMsg] = useState('');
-  const [userName, setUserName] = useState('');
-  const [passWord, setPassWord] = useState('');
-  const [roleAccount, setRoleAccount] = useState({ value: '', label: '' });
+  const [statusMsg, setStatusMsg] = useState("");
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [roleAccount, setRoleAccount] = useState({ value: "", label: "" });
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -29,14 +29,14 @@ function Login() {
 
     try {
       const response = await fetch(
-        'https://team9testwebapp-h3b5c7gqgbeqhxgp.swedencentral-01.azurewebsites.net/api/login',
+        "https://team9testwebapp-h3b5c7gqgbeqhxgp.swedencentral-01.azurewebsites.net/api/login",
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(newUser),
-        }
+        },
       );
 
       if (!response.ok) {
@@ -44,28 +44,29 @@ function Login() {
       }
 
       const data = await response.json();
-      console.log('Success:', data);
+      console.log("Success:", data);
 
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('username', data.UserName);
-      localStorage.setItem('role', data.Role);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("username", data.UserName);
+      localStorage.setItem("role", data.Role);
+      localStorage.setItem("id", data.ID);
 
-      setStatusMsg('Välkommen in!');
-      setUserName('');
-      setPassWord('');
-      setRoleAccount({ value: '', label: '' });
+      setStatusMsg("Välkommen in!");
+      setUserName("");
+      setPassWord("");
+      setRoleAccount({ value: "", label: "" });
       setLoading(false);
 
-      if (data.Role === 'receiver') {
-        navigate('/recipient');
-      } else if (data.Role === 'driver') {
-        navigate('/driver');
-      } else if (data.Role === 'sender') {
-        navigate('/sender');
+      if (data.Role === "receiver") {
+        navigate("/recipient");
+      } else if (data.Role === "driver") {
+        navigate("/driver");
+      } else if (data.Role === "sender") {
+        navigate("/sender");
       }
     } catch (error) {
-      console.error('Error:', error);
-      setStatusMsg('Registrering misslyckades. Försök igen.');
+      console.error("Error:", error);
+      setStatusMsg("Registrering misslyckades. Försök igen.");
       setLoading(false);
     }
   }
@@ -73,48 +74,48 @@ function Login() {
   return (
     <>
       <HeaderNavigation />
-      <main className='p-8 flex flex-col justify-center items-center gap-8'>
+      <main className="p-8 flex flex-col justify-center items-center gap-8">
         <BackArrow />
 
-        <div className='text-center'>
-          <h1 className='text-4xl font-bold text-text-dark mb-2'>Välkommen!</h1>
-          <p className='text-lg text-text-dark opacity-80'>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-text-dark mb-2">Välkommen!</h1>
+          <p className="text-lg text-text-dark opacity-80">
             Logga in för att fortsätta till din profil
           </p>
         </div>
 
         <Input
-          label='Email'
-          name='email'
-          id='email'
-          type='email'
-          className='max-sm:w-full'
+          label="Email"
+          name="email"
+          id="email"
+          type="email"
+          className="max-sm:w-full"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
         />
 
         <Input
-          label='Lösenord'
-          name='password'
-          id='password'
-          type='password'
-          className='max-sm:w-full'
+          label="Lösenord"
+          name="password"
+          id="password"
+          type="password"
+          className="max-sm:w-full"
           value={passWord}
           onChange={(e) => setPassWord(e.target.value)}
         />
 
         <Dropdown
           options={[
-            { value: 'driver', label: 'Driver' },
-            { value: 'sender', label: 'Sender' },
-            { value: 'receiver', label: 'Receiver' },
+            { value: "driver", label: "Driver" },
+            { value: "sender", label: "Sender" },
+            { value: "receiver", label: "Receiver" },
           ]}
           selectedValue={roleAccount.value}
           onSelect={(o) => setRoleAccount(o)}
-          placeholder='Välj kontotyp'
-          className='
+          placeholder="Välj kontotyp"
+          className="
             w-100
-            max-sm:w-full'
+            max-sm:w-full"
         />
 
         {loading ? (
@@ -122,17 +123,17 @@ function Login() {
         ) : (
           <PrimaryButton
             fullWidth={true}
-            text='Logga in'
+            text="Logga in"
             onClick={handleSubmit}
           />
         )}
-        <p className='font-bold'>{statusMsg}</p>
-        <div className='text-center'>
-          <p className='text-text-dark '>
-            Har du inget konto än?{' '}
+        <p className="font-bold">{statusMsg}</p>
+        <div className="text-center">
+          <p className="text-text-dark ">
+            Har du inget konto än?{" "}
             <Link
-              to='/sign-up'
-              className='underline font-bold hover:underline transition-colors'
+              to="/sign-up"
+              className="underline font-bold hover:underline transition-colors"
             >
               Registrera dig här
             </Link>
